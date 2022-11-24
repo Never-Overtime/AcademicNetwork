28689d1187b1d59c8b0da377cc8fc9f2cc919911
913-Diaconu-Ana <daie2909@scs.ubbcluj.ro>
2022-11-24 21:25:18 +0200

    StaffController

---
+++ AcademicWebApp/src/main/java/com/example/AcademicWebApp/Controllers/RestAPI/APIs/StaffController.java
@@ -0,0 +1,30 @@
+package com.example.AcademicWebApp.Controllers.RestAPIs;
+
+
+import com.example.AcademicWebApp.Repositories.UsersRepo;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+import java.util.Objects;
+
+
+@RestController
+@RequestMapping("/")
+public class StaffController  {
+
+    @GetMapping("/{username}/Staff")
+    public UserEntity getStudent(@PathVariable("username") String username, HttpServletRequest request)
+    {
+        //return "Hello, " + user. + "! You are a " + request.getAttribute("role");
+        return (UserEntity) request.getAttribute("userEntity");
+    }
+    
+}

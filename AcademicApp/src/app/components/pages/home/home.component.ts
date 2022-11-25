4c9c14d5e70c8661abc09bdee46f7abb244f2193
917_Zachman_Roxana <zrie3047@scs.ubbcluj.ro>
2022-11-25 06:47:43 +0200

    Add Homepage Frontend

--- AcademicApp/src/app/components/pages/home/home.component.ts
+++ AcademicApp/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LOGO_WIDTH, PAGE_PADDING, CONTENT_PADDING } from 'src/app/constants/sizes';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  logoWidth = LOGO_WIDTH
+  pagePadding = PAGE_PADDING
+  contentPadding = CONTENT_PADDING
 
   constructor() { }
 

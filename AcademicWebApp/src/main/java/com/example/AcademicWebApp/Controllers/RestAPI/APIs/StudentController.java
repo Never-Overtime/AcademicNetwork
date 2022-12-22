15df5bef3be9ead861353db48e459639839c3b00
913-Diaconu-Ana <daie2909@scs.ubbcluj.ro>
2022-12-22 21:39:01 +0200

    Enhanced student controller

--- AcademicWebApp/src/main/java/com/example/AcademicWebApp/Controllers/RestAPI/APIs/StudentController.java
+++ AcademicWebApp/src/main/java/com/example/AcademicWebApp/Controllers/RestAPI/APIs/StudentController.java
@@ -1,29 +1,20 @@
 package com.example.AcademicWebApp.Controllers.RestAPIs;
 
 
-import com.example.AcademicWebApp.Models.Student;
-import com.example.AcademicWebApp.Models.StudentData;
+import com.example.AcademicWebApp.Models.*;
 import com.example.AcademicWebApp.Repositories.StudentRepo;
-import com.example.AcademicWebApp.Repositories.UsersRepo;
 import com.example.AcademicWebApp.Services.StudentService;
 import lombok.RequiredArgsConstructor;
-import net.bytebuddy.asm.Advice;
-import net.bytebuddy.implementation.bind.annotation.Origin;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequiredArgsConstructor
 //@RequestMapping("/")
 public class StudentController {
@@ -49,9 +40,9 @@ public class StudentController {
     }
 
     @GetMapping("/student/{username}")
-    public String getStudentByUsername(@PathVariable("username") String username)
+    public Student getStudentByUsername(@PathVariable("username") String username)
     {
-        return studentRepo.findById(username).toString();
+        return studentRepo.findByUsername(username);
     }
 
     @GetMapping("/student/group1/{group1}")
@@ -61,17 +52,18 @@ public class StudentController {
     }
 
     @GetMapping("/student/group2/{group2}")
-    public String getStudentsBySecondGroup(@PathVariable("group2") String group2)
+    public List<Student> getStudentsBySecondGroup(@PathVariable("group2") String group2)
     {
-        return studentRepo.findBySecondGroup(Integer.valueOf(group2)).toString();
+        return studentRepo.findBySecondGroup(Integer.valueOf(group2));
     }
 
-    @GetMapping("/student/name/{name}")
-    public List<Student> getStudentByName(@PathVariable("name") String name)
-    {
-        return studentRepo.findByName(name);
-    }
+//    @GetMapping("/student/name/{name}")
+//    public List<Student> getStudentByName(@PathVariable("name") String name)
+//    {
+//        return studentRepo.findByName(name);
+//    }
 
+    // (✿◠‿◠)
     @PostMapping(value = "/student/add",
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public Student addStudent(@RequestBody StudentData student)
@@ -80,9 +72,81 @@ public class StudentController {
         return studentService.saveStudent(student);
     }
 
+
+
+    @GetMapping("/student/getCoursesFirstGroup/{username}")
+    public List<Course> getCoursesForStudentFirstGroup(@PathVariable(name = "username") String username)
+    {
+        return studentService.getCoursesForFirstGroup(username);
+
+    }
+
+    @GetMapping("/student/getCoursesSecondGroup/{username}")
+    public List<Course> getCoursesForStudentSecondGroup(@PathVariable(name = "username") String username)
+    {
+        return studentService.getCoursesForSecondGroup(username);
+
+    }
+
+    //DONE TODO #1 get api -  make api to get optionals based on faculty, year
+    @GetMapping("/student/getOptionalsByFacultyAndYear")
+    public List<Course> getOptionalsByFacultyAndYear(@RequestBody OptionalCourseData data)
+    {
+        return studentService.getOptionalsBasedOnFacultyAndYear(data);
+    }
+
+    //DONE TODO #2 get api - make api to get all faculties (as faculties)
+    @GetMapping("/student/getFaculties")
+    public List<Faculty> getFacultiesForStudent()
+    {
+        return studentService.getFaculties();
+    }
+
+
+    //DONE TODO #3 post api - list of courses, 100% optionals
+    //DONE TODO #4 service stuff - insert in optional_course_enrollment data based on the student and the list from #3
+    @PostMapping(value="/student/sendOptionalsPreferences/{username}",
+            consumes = "application/json",
+            produces = "application/json")
+    public List<OptionalCourseEnrollment> sendOptionalsPreferences(@RequestBody List<Course> courses, @PathVariable(name = "username") String username)
+    {
+        return studentService.sendOptionalsPreferences(courses, username);
+    }
+
+
+    //DONE TODO #5 get api - return list of all courses in which the student is enrolled (the first part is already implemented above these TODOS, hutsu) , including the optional one
+    @GetMapping("/student/getAllCoursesForWhichEnrolled/{username}")
+    public List<Course> getAllCoursesForWhichEnrolled(@PathVariable(name = "username") String username)
+    {
+        return studentService.getAllCoursesForWhichEnrolled(username);
+    }
+
+
+
+    //DONE TODO #n get api - get faculties+years that the student is enrolled in, based on username(cookie)
+    @GetMapping("/student/getFacultiesAndYears/{username}")
+    public List<FacultyAndYearData> getFacultiesAndYears(@PathVariable(name = "username") String username)
+    {
+        return studentService.getFacultiesAndYears(username);
+    }
+
+    //DONE TODO #n+1 get api - get courses+grades based on faculty and year (plus username from cookie) + (optional course + grade)
+    @PostMapping(value="/student/getGrades/{username}",
+            consumes = "application/json",
+            produces = "application/json")
+    public List<CourseGradeData> getGrades(@PathVariable(name = "username") String username,@RequestBody FacultyAndYearData data)
+    {
+        return studentService.getGrades(username, data);
+    }
+    //TODO DEADLINE 21 MAY 13:00 (all done at 3:26)
+    //(✿◠‿◠)
+
+
     public String sayHello(UserEntity user)
     {
         return "You are a " + user.getRole() + ". This should be that page where you can edit your profile and etc.";
     }
+
+
 
 }

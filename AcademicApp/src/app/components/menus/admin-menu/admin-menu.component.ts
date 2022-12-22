7f7110253601be79210f22d2e01d9e7e82acce36
917_Zachman_Roxana <zrie3047@scs.ubbcluj.ro>
2022-12-22 17:43:39 +0200

    Added menus

--- AcademicApp/src/app/components/menus/admin-menu/admin-menu.component.ts
+++ AcademicApp/src/app/components/menus/admin-menu/admin-menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { adminMenuData } from './adminMenuData';
+import { SIDEMENU_WIDTH, SIDEMENU_PADDING } from 'src/app/constants/sizes';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-admin-menu',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-menu.component.css']
 })
 export class AdminMenuComponent implements OnInit {
+  navData = adminMenuData;
+  sidemenuWidth = SIDEMENU_WIDTH;
+  sidemenuPadding = SIDEMENU_PADDING;
 
-  constructor() { }
+  @Input() name : string = 'Name Surnameeeeeeeeeeeeeeeee';
+
+
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    console.log("button pressed");
+    this.cookieService.delete('username');
   }
 
 }

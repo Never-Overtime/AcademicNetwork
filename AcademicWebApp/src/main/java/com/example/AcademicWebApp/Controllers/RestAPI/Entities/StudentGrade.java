0f806fed6ae2ed369332961bec83db4ab5ca8692
913-Diaconu-Ana <daie2909@scs.ubbcluj.ro>
2022-12-22 21:45:20 +0200

    Create StudentGrade.java

---
+++ AcademicWebApp/src/main/java/com/example/AcademicWebApp/Controllers/RestAPI/Entities/StudentGrade.java
@@ -0,0 +1,22 @@
+
+package com.example.AcademicWebApp.Controllers.RestAPIs.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentGrade {
+
+    private String username;
+    private String name;
+    private int group;
+    private int cid;
+    private int gradeValue;
+
+
+}

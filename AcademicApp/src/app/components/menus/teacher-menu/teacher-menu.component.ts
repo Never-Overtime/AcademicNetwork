649ef439f59f54cf15a35bfbb0782812e164b61c
DianaD <diana_draghiciu@yahoo.com>
2023-01-21 13:55:29 +0200

    implemented teacher menu and formatted some lines

--- AcademicApp/src/app/components/menus/teacher-menu/teacher-menu.component.ts
+++ AcademicApp/src/app/components/menus/teacher-menu/teacher-menu.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { teacherMenuData, chiefMenuData } from './teacherMenuData';
 import { SIDEMENU_WIDTH, SIDEMENU_PADDING } from 'src/app/constants/sizes';
 import {CookieService} from "ngx-cookie-service";
+import {FullName} from "../../../entities/fullName";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-teacher-menu',
@@ -13,17 +15,37 @@ export class TeacherMenuComponent implements OnInit {
   chiefData = chiefMenuData;
   sidemenuWidth = SIDEMENU_WIDTH;
   sidemenuPadding = SIDEMENU_PADDING;
-
-  @Input() name : string = 'Name Surnameeeeeeeeeeeeeeeee';
-
-  constructor(private cookieService: CookieService) { }
+  style: string = ''
+  name : string = '';
+  role: string = '';
+  chiefHidden: boolean = false;
+  constructor(private cookieService: CookieService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getFullName();
+
+    if(this.cookieService.get("role") == 'teacher'){
+      this.chiefHidden = true;
+      this.style = "gap: 6rem"
+    }
+    else{
+      this.style = "gap: 1.6rem"
+    }
+
+    this.role = this.cookieService.get("role");
+  }
+
+  getFullName(){
+    this.http.get<FullName>("http://localhost:8080/userdata/" + this.cookieService.get("username") + "/" +
+      this.cookieService.get("username"))
+      .subscribe((response:FullName) => {
+        this.name = response.fullname;
+      });
   }
 
   logout(){
-    console.log("button pressed");
     this.cookieService.delete('username');
+    this.cookieService.delete('role');
   }
 
 }

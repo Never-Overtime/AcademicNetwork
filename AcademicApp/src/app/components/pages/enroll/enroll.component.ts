f6e044a5b6f0ff8b6f7bd0a572620cca42bdd3cf
917_Zachman_Roxana <zrie3047@scs.ubbcluj.ro>
2023-01-21 22:21:56 +0200

    Added enroll

--- AcademicApp/src/app/components/pages/enroll/enroll.component.ts
+++ AcademicApp/src/app/components/pages/enroll/enroll.component.ts
@@ -1,17 +1,22 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {TableComponent} from "../../table/table.component";
-import {DropdownComponent} from "../../dropdown/dropdown.component";
-import {FacultyAndYearData} from "../../../entities/facultyAndYearData";
-import {StudentData} from "../../../entities/studentData";
-import {CONTENT_PADDING, LOGO_WIDTH, PAGE_PADDING} from "../../../constants/sizes";
-import { CookieService } from 'ngx-cookie-service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApisService } from 'src/app/apis/apis.service';
+import { LOGO_WIDTH, PAGE_PADDING, CONTENT_PADDING } from 'src/app/constants/sizes';
+import { FacultyAndYearData } from 'src/app/entities/facultyAndYearData';
+import { TABLE_TEST_CURRICULUM_DATA } from 'src/app/testing-dashboard/testingData';
+import { DropdownComponent } from '../../dropdown/dropdown.component';
+import { TableComponent } from '../../table/table.component';
+import { StudentData } from 'src/app/entities/studentData';
+import { CookieService } from 'ngx-cookie-service';
+import { Student } from 'src/app/entities/student';
 
 @Component({
   selector: 'app-enroll',
   templateUrl: './enroll.component.html',
   styleUrls: ['./enroll.component.css']
 })
+
+
+
 export class EnrollComponent implements OnInit {
   logoWidth = LOGO_WIDTH
   pagePadding = PAGE_PADDING
@@ -40,33 +45,34 @@ export class EnrollComponent implements OnInit {
   ngOnInit(): void {
 
     this.apiService.getStudentByUsername().subscribe((s) => {
-      if(s.group1 != null){ //  this means he is already in a faculty
-        this.apiService.getFacultiesAndYearsForStudent().subscribe((fac : FacultyAndYearData[]) =>{
-          fac.forEach((value, index) =>{
-            this.tableData.push(value);
-            this.table.changeRowsData(this.tableData);
+      if(s.group1 != null){
+          this.apiService.getFacultiesAndYearsForStudent().subscribe((fac : FacultyAndYearData[]) =>{
+            fac.forEach((value, index) =>{
+              this.tableData.push(value);
+              this.table.changeRowsData(this.tableData);
+            })
           })
-        })
-        this.disableButtons = true;
+          this.disableButtons = true;
+          this.alertUserThatHeIsDone();
+
       }
     })
 
     this.apiService.getAllFaculties().subscribe((result) => {
       let array : any = [];
-      // creating the array for the faculty dropdown element
       result.forEach((value, index) =>{
         let obj = {"id":index, "name": value.name, "noOfYears": value.noyears};
         array.push(obj);
       })
-      // setting the options for the dropdown faculty
       this.facultiesOptions = array;
     });
   }
 
+  alertUserThatHeIsDone(){
+    alert("You have already chosen your enrollments. If you want to change them, contact administration.")
+  }
+
   onFacultyOptionChanged(){
-    /**
-     * @TO_DO
-     * */
     let obj = this.facultyDd.getSelectedObject()[0];
     this.yearsOptions = [];
     let i = 0;
@@ -77,19 +83,15 @@ export class EnrollComponent implements OnInit {
   }
 
   onClickAddFacultyToTable(){
-    /**
-     * @TO_DO
-     * */
-
-    // cheking if there are already 2 faculties selected
     if(this.tableData.length == 2){
       alert("You can enroll to maximum 2 years.");
     }
     else{
+
+
       let objFac = this.facultyDd.getSelectedObject()[0];
       let objYear = this.yearDd.getSelectedObject()[0];
 
-      // checking if the faculty selected isnt already in it
       let okayToInsert : number = 1;
       this.tableData.forEach((model) =>{
         if(model.name == objFac["name"] && model.year == objYear["year"]){
@@ -110,7 +112,9 @@ export class EnrollComponent implements OnInit {
           this.table.changeRowsData(this.tableData);
         }
       }
+
     }
+
   }
 
   onClickClear(){
@@ -119,9 +123,6 @@ export class EnrollComponent implements OnInit {
   }
 
   onClickSend(){
-    /**
-     * @TO_DO
-     * */
     let data = this.table.getAllRowsData();
     let studentData: StudentData = new StudentData(this.cookieService.get("username"), "", 0, "", 0);
 
@@ -137,7 +138,11 @@ export class EnrollComponent implements OnInit {
       }
     }
     this.apiService.postEnrollStudent(studentData).subscribe();
-    alert("Faculties sent for enrollment!")
+    alert("Faculties sent for enrollment! If you want to change them, contact administration.")
     this.disableButtons = true;
   }
+
+
+
+
 }

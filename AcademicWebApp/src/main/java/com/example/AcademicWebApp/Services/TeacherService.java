4e2c53a5d28bb299346f5861e0a44a81b1da9bac
913 Stefan Alexandru Herlea <herleastefan@yahoo.com>
2023-01-21 23:59:10 +0200

    Updated repos and fixed some bugs

--- AcademicWebApp/src/main/java/com/example/AcademicWebApp/Services/TeacherService.java
+++ AcademicWebApp/src/main/java/com/example/AcademicWebApp/Services/TeacherService.java
@@ -7,10 +7,7 @@ import com.example.AcademicWebApp.Repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service("teacherService")
 public class TeacherService {
